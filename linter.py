af4d00c3e797ed2f8c7e4bb8a9ac78b9f7bdeae5
jasjuang <jasjuang@gmail.com>
2016-10-14 12:20:12 -0700

    initial commit

---
+++ linter.py
@@ -0,0 +1,32 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by hypevr,,,
+# Copyright (c) 2016 hypevr,,,
+#
+# License: MIT
+#
+
+"""This module exports the Lesshint plugin class."""
+
+from SublimeLinter.lint import NodeLinter, util
+
+
+class Lesshint(NodeLinter):
+    """Provides an interface to lesshint."""
+
+    syntax = 'less'
+    cmd = 'lesshint @'
+    executable = None
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 2.1.1'
+    regex = r'^.+?:.+?: line (?P<line>\d+), col (?P<col>\d+),(?P<message>.+)'
+    multiline = False
+    line_col_base = (1, 1)
+    tempfile_suffix = 'less'
+    error_stream = util.STREAM_BOTH
+    selectors = {}
+    word_re = None
+    defaults = {}
